f072f5328f23c1cf690d6301bf550afb3ffd21c9
CRISTHIAN WU <cristhian.wu@utp.ac.pa>
2025-06-12 22:17:48 -0500

    register funcional

--- backend/myapp/accounts/views.py
+++ backend/myapp/accounts/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db import transaction
+from .models import user_info_extend
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+from supertokens_python.recipe.emailpassword.syncio import sign_up, sign_in, update_email_or_password, \
+    reset_password_using_token
+from supertokens_python.recipe.userroles.syncio import add_role_to_user
+from supertokens_python.recipe.session.syncio import create_new_session, get_session, revoke_session
+from supertokens_python.recipe.emailpassword.interfaces import RecipeUserId, WrongCredentialsError, \
+    EmailAlreadyExistsError
+from supertokens_python.recipe.emailverification.asyncio import send_email_verification_email, is_email_verified
+
+# Error handling imports
+from supertokens_python.recipe.emailpassword.interfaces import (
+    UpdateEmailOrPasswordOkResult, PasswordPolicyViolationError,
+    PasswordResetTokenInvalidError, UnknownUserIdError)
+
+from supertokens_python.recipe.emailverification.interfaces import CreateEmailVerificationTokenOkResult, \
+    SendEmailVerificationEmailAlreadyVerifiedError, CreateEmailVerificationTokenEmailAlreadyVerifiedError, \
+    VerifyEmailUsingTokenOkResult, VerifyEmailUsingTokenInvalidTokenError
+
+class UserRegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
+        tenant_id = "public"
+        app_id = "public"
+
+        try:
+            if user_info_extend.objects.filter(id_document=request.data['id_document']).exists():
+                return Response({'error': 'This ID is already used'}, status=status.HTTP_409_CONFLICT)
+        
+            # SuperTokens register
+            new_user = sign_up(
+                tenant_id=tenant_id,
+                email=request.data['email'],
+                password=request.data['password']
+            )
+
+            # If email is already in use
+            if isinstance(new_user, EmailAlreadyExistsError):
+                return Response({
+                    'error': 'El correo ingresado ya está en uso',
+                    'status': EmailAlreadyExistsError.status
+                }, status=status.HTTP_409_CONFLICT)
+            
+            # Add role to user
+            add_role_to_user(
+                tenant_id=tenant_id,
+                user_id=new_user.user.id,
+                role=request.data['role']
+            )
+
+            # Save user extended information
+            with transaction.atomic():
+                user_info_extend.objects.create(
+                    app_id=app_id,
+                    user_id=new_user.user.id,
+                    name=request.data['name'],
+                    last_name=request.data['last_name'],
+                    id_document=request.data['id_document']
+                )
+
+            return Response({
+                'message': 'Usuario registrado con éxito',
+                'user_id': new_user.user.id
+            }, status=status.HTTP_201_CREATED)
+        
+        except Exception as e:
+            print(f"[Error] Registro: {e}")
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- backend/myapp/myapp/urls.py
+++ backend/myapp/myapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import UserRegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user_register/', UserRegisterView.as_view())
 ]
